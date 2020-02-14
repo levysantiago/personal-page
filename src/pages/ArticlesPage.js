6c44b72e971f6e72d9e99e809dbaa4f87c066703
Levysantiago <levyssantiago@gmail.com>
2020-02-14 19:35:05 -0300

    using internationalization

--- src/pages/ArticlesPage.js
+++ src/pages/ArticlesPage.js
@@ -4,28 +4,31 @@ import ListArticles from "../components/ListArticles";
 import PageHeader from "../components/PageHeader";
 import Blockquote from "../components/Blockquote";
 import Footer from "../components/Footer";
-import dict from "../language/pt";
+import dict from "../language/info";
+import lang from "../language/en";
 
 class ArticlesPage extends Component {
   render() {
     return (
       <div>
-        <NavBar blackStyle={true} />
+        <NavBar blackStyle={true} lang={lang} />
         <div className="container">
           <PageHeader
-            title="Publicações"
-            description="Nesta página são apresentados os artigos publicados que escrevi
-            juntamente com outros autores. Grande parte destes autores fazem
-            parte do grupo LIF - Laboratório de Internet do Futuro."
+            title={lang.articlespage.title}
+            description={lang.articlespage.description}
           />
 
-          <ListArticles long={"12"} list={dict.articles.reverse()} />
+          <ListArticles
+            long={"12"}
+            list={dict.articles.reverse()}
+            lang={lang}
+          />
           <Blockquote
             phrase={dict.blockquotes.two.phrase}
             author={dict.blockquotes.two.author}
           />
         </div>
-        <Footer />
+        <Footer lang={lang} />
       </div>
     );
   }
