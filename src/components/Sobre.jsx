fe0ac5c126b064bfe8c6a5cbfd5fff132f197c51
Levysantiago <levyssantiago@gmail.com>
2020-02-14 16:29:47 -0300

    fetch master

--- src/components/Sobre.jsx
+++ src/components/Sobre.jsx
@@ -2,45 +2,12 @@ import React, { Component } from "react";
 
 class Sobre extends Component {
   /*textStyle = {
-    fontFamily: "Quicksand"
-  };*/
+      fontFamily: "Quicksand"
+    };*/
 
   render() {
-    return (
-      <div className="row">
-        {/*<h2>Sobre</h2>*/}
-        <article>
-          <p>
-            Eu sou Levy. Sou originariamente brasileiro. Considero-me uma pessoa
-            calma, amigável, curiosa e dedicada. Gosto de viajar, de aprender
-            coisas novas e de conhecer novas pessoas.
-          </p>
-          <br />
-          <p>
-            Hoje sou bacharelado em Ciência da Computação na UESC -{" "}
-            <a
-              href="http://www.uesc.br/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Universidade Estadual de Santa Cruz
-            </a>
-            . Faço parte do grupo LIF - Laboratório de Internet do Futuro, um
-            grupo iniciado na UESC que envolve tanto estudantes como
-            professores. Muitos dos meus projetos e publicações foram
-            desenvolvidos juntamente com este grupo. Atualmente estou
-            trabalhando em um projeto que envolve criação de contratos
-            inteligentes utilizando a rede Ethereum.
-          </p>
-          <br />
-          <p>
-            Tenho experiência em Programação Web, Internet das Coisas,
-            Ontologias, Blockchain e outras áreas e tecnologias.
-          </p>
-          <br />
-        </article>
-      </div>
-    );
+    const { lang } = this.props;
+    return <div>{lang.about.description}</div>;
   }
 }
 
