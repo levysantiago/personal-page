d6e387adba6f1ff62c0a5df042bf0d23016e4aa5
Levysantiago <levyssantiago@gmail.com>
2020-02-14 19:34:17 -0300

    revising pt.js

--- src/language/pt.js
+++ src/language/pt.js
@@ -1,143 +1,68 @@
-module.exports = {
-  articles: [
-    {
-      key: 1,
-      title:
-        "Analise de softwares de virtualização de objetos eletrônicos para utilização em projetos de ensino que utilizem o conceito de Internet das Coisas: Protoboard Virtual e Plataforma Arduíno.",
+import React from "react";
 
-      year: "2017",
-      place:
-        "XVII ESCOLA REGIONAL DE COMPUTAÇÃO BAHIA - ALAGOAS - SERGIPE, Cruz das Almas",
-      proceeding:
-        "Anais Workshop de Educação e Informática Bahia-Alagoas-Sergipe (WEIBASE), p. 46-55",
-      link:
-        "https://drive.google.com/file/d/1m7rEnwgkHGoOUowkV6Vdm-x7T02h8TRI/view?usp=sharing"
-    },
-    {
-      key: 2,
-      title:
-        "Redes Sociais na educação - Revisão de softwares de Redes Sociais como ferramentas educaionais",
-
-      year: "2017",
-      place:
-        "XVII ESCOLA REGIONAL DE COMPUTAÇÃO BAHIA - ALAGOAS - SERGIPE, Cruz das Almas",
-      proceeding:
-        "Anais Workshop de Educação e Informática Bahia-Alagoas-Sergipe (WEIBASE), p. 36-45",
-      link:
-        "https://drive.google.com/file/d/1m7rEnwgkHGoOUowkV6Vdm-x7T02h8TRI/view?usp=sharing"
-    },
-    {
-      key: 3,
-      title:
-        "Avaliação de linguagens visuais de programação no ensino médio a partir da utilização do conceito de Robótica Pedagógica",
-
-      year: "2017",
-      place: "VI Congresso Brasileiro de Informática na Educação, Recife",
-      proceeding:
-        "Anais dos Workshops do Congresso Brasileiro de Informática na Educação (CBIE), p. 962-971",
-      link: "http://br-ie.org/pub/index.php/wcbie/article/view/7485"
-    },
-    {
-      key: 4,
-      title:
-        "I2oTegrator: a Service-Oriented IoT Middleware for Intelligent Object Management",
-
-      year: "2018",
-      place:
-        "2018 IEEE Symposium on Computers and Communications (ISCC), Natal",
-      proceeding:
-        "2018 IEEE Symposium on Computers and Communications (ISCC), p. 00174",
-      link: "https://ieeexplore.ieee.org/document/8538541"
-    },
-    {
-      key: 5,
-      title: "I2oTology - Tracking-Oriented Ontology",
-
-      year: "2018",
-      place: "XI Seminar on Ontology Research in Brazil, São Paulo",
-      proceeding:
-        "Proceedings of the XI Seminar on Ontology Research in Brazil and II Doctoral and Masters Consortium on Ontologies",
-      link: "http://ceur-ws.org/Vol-2228/short5.pdf"
-    },
-    {
-      key: 6,
-      title: "IoTCocoa - an IoT platform to assist gourmet cocoa production",
-
-      year: "2019",
-      place:
-        "IEEE LatinAmerican Conference on Communications (LATINCOM), Salvador",
-      proceeding:
-        "IEEE Latin-American Conference on Communications (LATINCOM), p. 1",
-      link: "https://ieeexplore.ieee.org/document/8937903"
-    }
-  ],
-
-  projects: [
-    {
-      key: 1,
-      title: "ta5ks",
-      abstract:
-        "Este é um aplicativo desenvolvido junto com amigos da faculdade. É um aplicativo que permite a criação e gerenciamento de tarefas (tasks).Ele foi desenvolvido utilizando Android Studio.Cada tarefa pode ser associada com um Projeto(ou tag) que representa a que a tarefa se refere.",
-      link: "https://github.com/Levysantiago/ta5ks"
-    },
-    {
-      key: 2,
-      title: "Math Kids",
-      abstract:
-        "Um jogo educativo para ajudar as crianças no aprendizado de contagem de elementos.O jogo implementa uma interface de drag and drop que contribui para a cordenação motora da criança.",
-      link: "https://github.com/Levysantiago/MathKids"
-    },
-    {
-      key: 3,
-      title: "IoT-Cocoa",
-      abstract:
-        "Um sistema para auxiliar a produção de cacau gourmet com suporte a blockchain. O link mostra uma página onde foram obtidos dados do processo de fermentação de uma remessa de cacau. Foram controladas a temperatura e humidade ambiente e também de dentro do cocho. O sistema conta com um hardware que comporta três reles para controlar uma fonte de ventilação, fonte de calor e misturador do coxo.",
-      link: "https://cacau.herokuapp.com/"
-    },
-    {
-      key: 4,
-      title: "BlockEGovMe",
-      abstract:
-        "Este foi um projeto de Iniciação Científica da Universidade Estadual de Santa Cruz (UESC) que visa a implementação de contratos inteligentes para o Governo Eletrônico (E-Gov). Foram desenvolvidos três contratos, um para a Carteira de Vacinação, outro para Registro de Imóvel e por último para Comprovante de Matrícula. O único contrato implementado para teste e integrado com um Middleware foi o contrato para Comprovante de Matrícula, para este, foi criada uma página para mostrar a o funcionamento do contrato a partir do Middleware.",
-      link: "https://github.com/Levysantiago/smartcontracts-for-egov"
-    }
-  ],
-  hellowtips: [
-    { key: 15, img: "dicas16.jpg" },
-    { key: 14, img: "dicas15.jpg" },
-    { key: 13, img: "dicas14.jpg" },
-    { key: 12, img: "dicas13.jpg" },
-    { key: 11, img: "dicas12.jpg" },
-    { key: 10, img: "dicas11.jpg" },
-    { key: 9, img: "dicas10.jpg" },
-    { key: 8, img: "dicas9.jpg" },
-    { key: 7, img: "dicas8.jpg" },
-    { key: 6, img: "dicas7.jpg" },
-    { key: 5, img: "dicas6.jpg" },
-    { key: 4, img: "dicas5.jpg" },
-    { key: 3, img: "dicas4.jpg" },
-    { key: 2, img: "dicas2.jpg" },
-    { key: 1, img: "dicas1.jpg" }
-  ],
-  blockquotes: {
-    one: {
-      phrase:
-        "Great things in business are never done by one person. They're done by a team of people.",
-      author: "Steve Jobs"
-    },
-    two: {
-      phrase:
-        "It’s fine to celebrate success, but it is more important to heed the lessons of failure.",
-      author: "Bill Gates"
-    },
-    three: {
-      phrase: "Codes are a puzzle. A game, just like any other game.",
-      author: "Alan Turing"
-    },
-    four: {
-      phrase:
-        "People won't have time for you if you are always angry or complaining.",
-      author: "Stephen Hawking"
-    }
+const lang = {
+  /**
+   * HOME PAGE
+   */
+  about: {
+    title: "Sobre",
+    description: (
+      <article>
+        <p>
+          Eu sou Levy. Sou originariamente brasileiro. Considero-me uma pessoa
+          calma, amigável, curiosa e dedicada. Gosto de viajar, de aprender
+          coisas novas e de conhecer novas pessoas.
+        </p>
+        <br />
+        <p>
+          Hoje sou bacharelado em Ciência da Computação na UESC -{" "}
+          <a
+            href="http://www.uesc.br/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Universidade Estadual de Santa Cruz
+          </a>
+          . Faço parte do grupo LIF - Laboratório de Internet do Futuro, um
+          grupo iniciado na UESC que envolve tanto estudantes como professores.
+          Muitos dos meus projetos e publicações foram desenvolvidos juntamente
+          com este grupo. Atualmente estou trabalhando em um projeto que envolve
+          criação de contratos inteligentes utilizando a rede Ethereum.
+        </p>
+        <br />
+        <p>
+          Tenho experiência em Programação Web, Internet das Coisas, Ontologias,
+          Blockchain e outras áreas e tecnologias.
+        </p>
+        <br />
+      </article>
+    )
+  },
+  languages: {
+    title: "Linguagens",
+    description: (
+      <article>
+        <p style={{ color: "white" }}>
+          Os gráficos abaixo demonstram as linguagens de programação as quais
+          adquiri experiência até hoje. As porcentagens representam o quanto
+          tenho de experiência e conhecimento em cada uma das linguagens. É uma
+          forma controversa, uma vez que conhecimento não se quantifica, porém é
+          direta e compreensível.
+        </p>
+      </article>
+    )
+  },
+  /**
+   * CONSTANTS
+   */
+  constants: {
+    place: "Local",
+    year: "Ano",
+    in: "Em",
+    read: "Ler",
+    moreArticles: "Veja mais publicações aqui",
+    moreProjects: "Veja mais projetos aqui"
   }
 };
+
+export default lang;
