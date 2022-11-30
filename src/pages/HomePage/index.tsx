bd19e07e8f1f895ff08d7c88bbe55f1a7c4d7a38
Levy Santiago <levyssantiago@gmail.com>
2022-11-30 17:07:23 -0300

    refactor: restarting project with Typescript

---
+++ src/pages/HomePage/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HomePage: React.FC = () => {
+  return (
+    <div>
+      <label>Website with Typescript</label>
+    </div>
+  );
+};
+
+export default HomePage;
