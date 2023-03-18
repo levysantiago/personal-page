4ab6390d2d997b9cc07b4ca22048dcaad5861585
Levy Santiago <levyssantiago@gmail.com>
2023-03-17 23:08:23 -0300

    refactor: removing unused pictures

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -12,20 +12,9 @@ import {
 import React, { useState } from "react";
 
 const Banner: React.FC = () => {
-  const [imageSelected, setImageSelected] = useState(true);
-
-  const images = {
-    true: require("../../assets/profile/me.jpg"),
-    false: require("../../assets/profile/avatar.svg").default,
-  };
-
-  const changeImage = () => {
-    setImageSelected(!imageSelected);
-  };
-
   return (
     <Container>
-      <ImgContainer onClick={changeImage}>
+      <ImgContainer>
         <ProfileImg />
       </ImgContainer>
       <TextContentContainer>
