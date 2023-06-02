01626ca81298636918358dbef215cda12a7858d7
Levy Santiago <levyssantiago@gmail.com>
2023-06-02 11:56:45 -0300

    refactor: starting migration to next

--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import Routes from "./routes";
-
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
-
-root.render(Routes);
